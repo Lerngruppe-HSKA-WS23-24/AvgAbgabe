176b5b8ce743fdafb1ec7674d5d533738d68a836
ahmado1310 <43607545+ahmado1310@users.noreply.github.com>
2023-12-15 11:42:23 +0100

    fix: /candidates and /employess are Back

--- server.js
+++ server.js
@@ -6,36 +6,27 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 
-// Funktion zur Überprüfung der Gehaltsvorstellungen der Kandidaten
-function validateCandidatesSalary(candidates) {
-  return candidates.filter(candidate => candidate.desired_salary > 100000);
+// Validierungsfunktion, die jetzt direkt das Anfrage-Objekt verwendet
+function validateCandidateRequest(requestData) {
+  return requestData.salary > 100000;
 }
 
-// Route zur Validierung der Gehälter
-server.post('/validate-salary', (req, res) => {
-  const candidates = router.db.get('candidates').value();
-  const highEarners = validateCandidatesSalary(candidates);
+// Route zur Validierung der Daten aus dem Formular
+server.post('/validate', (req, res) => {
+  // Die Anfrage-Daten direkt aus req.body extrahieren
+  const requestData = req.body;
 
-  if (highEarners.length > 0) {
+  // Validierung der Daten aus dem Formular
+  const isSalaryTooHigh = validateCandidateRequest(requestData);
+
+  if (isSalaryTooHigh) {
+    console.log("Ein Kandidat möchte mehr als 100000€ verdienen.");
     res.status(400).json({
-      message: 'Es gibt Kandidaten, die mehr als 100000€ verdienen möchten.',
-      highEarners
+      message: 'Ein Kandidat möchte mehr als 100000€ verdienen.',
+      requestData
     });
   } else {
-    res.json({ message: 'Alle Kandidaten haben akzeptable Gehaltswünsche.' });
-  }
-});
-
-// Neue Route zur Überprüfung, ob ein Kandidat vorhanden ist
-server.get('/candidates/check', (req, res) => {
-  const { surname } = req.query;
-  const candidates = router.db.get('candidates').value();
-  const candidateExists = candidates.some(candidate => candidate.surname === surname);
-
-  if (candidateExists) {
-    res.json({ message: 'Der Kandidat ist vorhanden.' });
-  } else {
-    res.status(404).json({ message: 'Der Kandidat wurde nicht gefunden.' });
+    res.json({ message: 'Die Gehaltsvorstellung des Kandidaten ist akzeptabel.' });
   }
 });
 
