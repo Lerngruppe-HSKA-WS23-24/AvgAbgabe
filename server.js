f1aa3c959d476d8b1aea6f559e3417e3b2575bdf
Leon Gauweiler <leon.gauweiler@gmail.com>
2023-12-17 16:49:05 +0100

    added: delete candidate & add employees

--- server.js
+++ server.js
@@ -6,30 +6,32 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 
-// Validierungsfunktion, die jetzt direkt das Anfrage-Objekt verwendet
-function validateCandidateRequest(requestData) {
-  return requestData.desired_salary > 100000;
-}
-
-// Route zur Validierung der Daten aus dem Formular
-server.post('/validate', (req, res) => {
-  // Die Anfrage-Daten direkt aus req.body extrahieren
-  const requestData = req.body;
-
-  // Validierung der Daten aus dem Formular
-  const isSalaryTooHigh = validateCandidateRequest(requestData);
-
-  if (isSalaryTooHigh) {
-    console.log("Ein Kandidat möchte mehr als 100000€ verdienen.");
-    res.status(400).json({
-      message: 'Ein Kandidat möchte mehr als 100000€ verdienen.',
-      requestData
-    });
+server.delete('/candidates/:c_id', (req, res) => {
+  const c_id = req.params.c_id;
+  const candidate = router.db.get('candidates').find({ c_id: c_id }).value();
+  if (candidate) {
+    router.db.get('candidates').remove({ c_id: c_id }).write();
+    res.status(200).json({ message: 'Kandidat erfolgreich gelöscht.' });
   } else {
-    res.json({ message: 'Die Gehaltsvorstellung des Kandidaten ist akzeptabel.' });
+    res.status(404).json({ message: 'Kandidat nicht gefunden.' });
   }
 });
 
+server.post('/employees', (req, res) => {
+  const employee = req.body;
+  if (employee) {
+    employee.id = findLastEmployee(router.db).id + 1;
+    router.db.get('employees').push(employee).write();
+    res.status(201).json({ message: 'Mitarbeiter erfolgreich erstellt.' });
+  } else {
+    res.status(400).json({ message: 'Ungültige Anfrage.' });
+  }
+});
+
+function findLastEmployee(db) {
+  const employees = db.get('employees').value();
+  return employees[employees.length - 1];
+}
 server.use(router);
 
 server.listen(3000, () => {
